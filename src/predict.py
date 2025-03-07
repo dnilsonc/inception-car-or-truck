043bfdba3466f6b5c071d8044b74f9fa5f164353
Dnilson Sousa <dnilsonsousaoficial17@gmail.com>
2025-03-07 16:20:02 -0300

    refactor prediction logic and add HTML/CSS/JS for vehicle classification interface

--- src/predict.py
+++ src/predict.py
@@ -1,15 +1,19 @@
 import tf_keras
+import tensorflow as tf
 import tensorflow_hub as hub
 import numpy as np
+
+import os
 
 from tensorflow.keras.preprocessing import image # type: ignore
 
 # Carregar o modelo salvo
 try:
-    model = tf_keras.models.load_model('models/model.keras', custom_objects={'KerasLayer': hub.KerasLayer})
+    model = tf_keras.models.load_model('models/car_or_truck.h5', custom_objects={'KerasLayer': hub.KerasLayer})
     print("Modelo carregado com sucesso!")
 except Exception as e:
     print(f"Erro ao carregar o modelo: {e}")
+    print(os.getcwd())
     exit(1)
 
 
@@ -17,7 +21,7 @@ def preprocess_image(img_path):
     """Carrega e pré-processa a imagem para a entrada do modelo."""
     try:
         img = image.load_img(img_path, target_size=(128, 128))  # Ajuste para o tamanho esperado
-        img_array = image.img_to_array(img) #/ 255.0  # Normalizar para [0,1]
+        img_array = image.img_to_array(img) / 255
         img_array = np.expand_dims(img_array, axis=0)  # Adiciona dimensão de batch
         return img_array
     except Exception as e:
@@ -25,20 +29,24 @@ def preprocess_image(img_path):
         return None
 
 
-def predict(img_path = "data/valid/Car/05118.jpeg"):
-    """Recebe uma imagem e retorna a classe prevista pelo modelo."""
+def predict(img_path="data/valid/Car/05118.jpeg"):
     img_array = preprocess_image(img_path)
-    
     if img_array is None:
         return {"error": "Falha no pré-processamento da imagem."}
     
     prediction = model.predict(img_array)
-
-    # Se for um problema binário (0 ou 1), aplicar threshold de decisão
-    predicted_class = 1 if prediction[0] > 0.5 else 0  
-
-    return {"prediction": predicted_class, "confidence": float(prediction[0])}
-
+    raw_prediction = float(prediction[0])
+    predicted_class = 1 if raw_prediction > 0.5 else 0
+    class_label = "Truck" if predicted_class == 1 else "Car"
+    
+    # Confiança ajustada para a classe prevista
+    confidence = raw_prediction if predicted_class == 1 else 1 - raw_prediction
+    
+    return {
+        "prediction": class_label,
+        "confidence": confidence,
+        "raw_output": raw_prediction  # Para debug
+    }
 
 # Exemplo de uso
 if __name__ == '__main__':
